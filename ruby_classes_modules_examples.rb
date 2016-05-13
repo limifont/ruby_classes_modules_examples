7eaf8fa7f1a9d67572917290cefc231323682ca6
Lindsey Font <lindsey.font@gmail.com>
2016-05-13 10:45:13 -0600

    Added gem files

--- ruby_classes_modules_examples.rb
+++ ruby_classes_modules_examples.rb
@@ -1,12 +1,21 @@
+require 'pry'
+require 'babbler'
+
+
 puts 'YAY Friday!'
 
 def keep_coding
   puts 'Pop question: Are you going to keep coding over the weekend? (yes / no)'
   input = gets.strip.downcase
+
+  # if input == 'yes' ? (puts 'Good answer'): (puts 'You serious, bro? Do you even code?') keep_coding
+
+
   if input == 'yes'
     puts 'Good answer!'
   else
-    puts 'You serious, bro? Do you even code?'
+  	puts Babbler.babble
+    # puts 'You serious, bro? Do you even code?'
     keep_coding
   end
 end
